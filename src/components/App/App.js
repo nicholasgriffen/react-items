a9282e5019797e163302cb9fee27d7d5a3a9e17d
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-11-20 10:48:54 -0700

    state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        { id: 0, item: 'Old AM radio', reason: 'Coast to Coast' },
+        { id: 1, item: 'Shovel', reason: 'Find underground telephone wires' },
+        { id: 2, item: 'Ham radio', reason: 'Talk to people' }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-       <p>My components here</p>      
+       <p>'???'</p>      
        </div>
     )
   }
